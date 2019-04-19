5dfac2b66a2a513ae4dc500d89f0d89c8dfe6a80
决心要Coding的Z少女 <36120585+JOINYYz@users.noreply.github.com>
2019-04-19 10:38:14 +0800

    Create backtrace.py

---
+++ code/backtrace.py
@@ -0,0 +1,37 @@
+n =       #  n元
+x = [0]*n # 初始化一个解
+X = []    # 一组解
+
+def conflict(k):
+  global 
+  
+  if 冲突：
+      return True
+  else: return False
+
+#子集树  
+def function(k):
+  global
+  
+  if k>=n: #超出最后的元素
+      print(x)
+      # X.append(x) 保存多组解
+  else:
+      for i in range(m): # m个状态
+          x[k] = i
+          if not conflict(k): #剪枝
+              function(k+1) #递归实现回溯
+              
+#排列树
+def function(k):
+  global
+  
+  if k>=n: #超出最后的元素
+      print(x)
+      # X.append(x) 保存多组解
+  else:
+      for i in range(m): # m个状态
+          x[k],x[i] =  x[i],x[k]
+          if not conflict(k): #剪枝
+              function(k+1) #递归实现回溯
+          x[i], x[k] = x[k], x[i] # 回溯
